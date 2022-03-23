800cff8561178c78abd3f6156b9b983cb3e5c52c
AdBirecki <abirecki@gmail.com>
2022-03-24 00:02:01 +0100

    Init

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Newtonsoft.Json;
+
+Console.WriteLine("Hello, World!");
+
+int[] array = new int[9] { 12, 523, 42, 53, 31, 32, 51, 112, 7 };
+BubbleSort(array);
+
+void BubbleSort(int[] arr)
+{
+    int iterationCount = array.Length;
+    while (iterationCount > 0)
+    {
+        for (int i = 0; i < arr.Length - 1; i++)
+        {
+            if (array[i] > array[i + 1])
+            {
+                Swap(ref array[i], ref array[i + 1]);
+            }
+        }
+        iterationCount--;
+    }
+}
+
+string json = JsonConvert.SerializeObject(array);
+Console.WriteLine(json);
+
+void Swap(ref int x, ref int y)
+{
+    (x, y) = (y, x);
+}
---
+++ SelectionSort/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int[] array = new int[] { 1, 2, 3, 5, 6, 7, 8 }
+int findmaximumIndex(int x)
+//https://www.geeksforgeeks.org/selection-sort/
+
+
+int findmaximumIndex(int x) { return x; }
